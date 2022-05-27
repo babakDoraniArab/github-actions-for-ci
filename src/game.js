dbd3432db6ddd572de911aab62dee8347f3bc9a1
babakDoraniArab <babak.dorani@gmail.com>
2022-05-27 13:03:10 +0300

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 export default class Game {
   constructor (p1, p2) {
     this.p1 = p1
-    this.p2 = 'Bananas'
+    this.p2 = p2
     this.board = [[null, null, null], [null, null, null], [null, null, null]]
     this.player = Math.random() < 0.5 ? this.p1 : this.p2
     this.sym = 'X'
